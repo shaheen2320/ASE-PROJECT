d6d090ac60fe2117c91bd41f0f458ce350765f60
shaheen memon <s.memon2294@student.leedsbeckett.ac.uk>
2023-12-22 01:04:42 +0000

    Xml comment added, unit test for draw rectangle addded.

---
+++ WindowsFormsApp1/drawingcommands_unit_test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("drawingcommands_unit_test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("drawingcommands_unit_test")]
+[assembly: AssemblyCopyright("Copyright © HP 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("bd5ac22b-b5f2-419a-85b3-8846686a54b8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ WindowsFormsApp1/drawingcommands_unit_test/draw_parser_class.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ASE_TASK_1;
+using System.Windows.Forms;
+
+namespace ASE_TASK_1.Tests
+{
+    [TestClass]
+    public class Draw_Parser_classTests
+    {
+        [TestMethod]
+        public void InvalidParameters_check_drawrectangle()
+        {
+           
+            PictureBox pictureBox = new PictureBox();
+            Draw_Parser_class drawParser = new Draw_Parser_class(pictureBox);
+            string command = "draw rectangle 20 40 60"; 
+
+            
+            try
+            {
+                drawParser.Drawing_Commands(command);
+                Assert.Fail("Unexpected ArgumentException thrown.");
+            }
+            catch (Exception ex)
+            {
+                
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+    }
+}
+
+
+        
+
