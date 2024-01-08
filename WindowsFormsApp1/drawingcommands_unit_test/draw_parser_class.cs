7642ef8f1177b633077a633ccc7d82410caa3f06
shaheen memon <s.memon2294@student.leedsbeckett.ac.uk>
2024-01-08 22:10:03 +0000

    xml commit and unit test working

--- WindowsFormsApp1/drawingcommands_unit_test/draw_parser_class.cs
+++ WindowsFormsApp1/drawingcommands_unit_test/draw_parser_class.cs
@@ -11,12 +11,12 @@ namespace ASE_TASK_1.Tests
         [TestMethod]
         public void InvalidParameters_check_drawrectangle()
         {
-           
+
             PictureBox pictureBox = new PictureBox();
             Draw_Parser_class drawParser = new Draw_Parser_class(pictureBox);
-            string command = "draw rectangle 20 40 60"; 
+            string command = "draw rectangle 20 40 60";
 
-            
+
             try
             {
                 drawParser.Drawing_Commands(command);
@@ -24,11 +24,32 @@ namespace ASE_TASK_1.Tests
             }
             catch (Exception ex)
             {
-                
+
                 Console.WriteLine(ex.ToString());
             }
         }
 
+        [TestMethod]
+        public void InvalidParameters_check_drawCircle()
+        {
+            // Arrange
+            PictureBox pictureBox = new PictureBox();
+            Draw_Parser_class drawParser = new Draw_Parser_class(pictureBox);
+            string command = "draw circle 30 50";
+
+            try
+            {
+                drawParser.Drawing_Commands(command);
+                Assert.Fail("Unexpected ArgumentException thrown.");
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.ToString());
+            }
+
+
+        }
     }
 }
 
